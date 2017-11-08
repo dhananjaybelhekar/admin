027247aa4f9ba3c7a5b22d20fa7ca47f75b584fd
dhananjay431 <dbel431@gmail.com>
2017-11-08 15:51:26 +0530

    add ng

--- newPrj/todo.js
+++ newPrj/todo.js
@@ -2,11 +2,16 @@ angular.module('todoApp', ['ui.router'])
 .config(function($stateProvider,$urlRouterProvider,$locationProvider) {
   $locationProvider.html5Mode(true);
   $urlRouterProvider.otherwise('/');
-  $stateProvider.state({
+  $stateProvider.state('home', {
     url: '/',
-    template: '<h1>dfdfsdf</h1>'
-  });
+    template: '<h1>dfdfsdf</h1>',
+//  templateUrl: 'partial-home-list.html',
+  controller:function($scope){ $scope.a1='Hii..';alert($scope.a1)}
+    });
 
 }).controller('TodoListController', function() {
     var todoList = this;
   });
+
+
+
