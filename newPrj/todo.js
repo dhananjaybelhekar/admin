f8869475e670962cc35f6fe5b2d542bd0ac06f89
dhananjay431 <dbel431@gmail.com>
2017-11-09 14:59:01 +0530

    angular module

--- newPrj/todo.js
+++ newPrj/todo.js
@@ -1,17 +1,59 @@
-angular.module('todoApp', ['ui.router'])
+
+angular.module('todoApp.login', ['ui.router'])
 .config(function($stateProvider,$urlRouterProvider,$locationProvider) {
   $locationProvider.html5Mode(true);
-  $urlRouterProvider.otherwise('/');
-  $stateProvider.state('home', {
-    url: '/',
-    template: '<h1>dfdfsdf</h1>',
-//  templateUrl: 'partial-home-list.html',
-  controller:function($scope){ $scope.a1='Hii..';alert($scope.a1)}
+  $urlRouterProvider.otherwise('/login');
+  $stateProvider.state('login', {
+    url: '/login',
+	templateUrl: 'login.html',
+  	controller:'todoApp.login.ctrl'
     });
 
-}).controller('TodoListController', function() {
+}).controller('todoApp.login.ctrl', function() {
     var todoList = this;
-  });
+    console.log('todoApp.login');
+});
 
 
+angular.module('todoApp.menu', ['ui.router','ckeditor'])
+.config(function($stateProvider,$urlRouterProvider,$locationProvider) {
+// $urlRouterProvider.otherwise('/menu');
+//   $locationProvider.html5Mode(true);
+  $stateProvider.state('menu', {
+    url: '/menu',
+    //template: '<h1>dfdfsdf</h1>',
+	templateUrl: 'menu.html',
+ 	controller:'todoApp.menu.ctrl'
+    })
+  	.state('menu.sub', {
+    url: '/sub',
+	templateUrl: 'sub.html',
+ 	controller:'sub.menu.ctrl'
+    });
 
+}).controller('todoApp.menu.ctrl', function() {
+    var todoList = this;
+     todoList.options = {
+        language: 'en',
+        allowedContent: true,
+        entities: false
+      };
+
+  // Called when the editor is completely ready.
+    todoList.onReady = function () {
+      console.log("onReady");
+    };
+    console.log('todoApp.menu.ctrl');
+}).controller('sub.menu.ctrl', function() {
+    var todoList = this;
+    console.log('sub.menu.ctrl');
+});
+
+
+angular.module('todoApp', [
+	'todoApp.login',
+	'todoApp.menu',
+	'ui.router']).controller('TodoListController', function() {
+    var todoList = this;
+    console.log('TodoListController');
+});
